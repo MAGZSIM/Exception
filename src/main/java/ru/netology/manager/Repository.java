c72de6b740b0aeee738f62db2312101dec32d67e
Maks S <Gmagz@yandex.ru>
2023-05-25 13:17:33 +0300

    Initial commit

---
+++ src/main/java/ru/netology/manager/Repository.java
@@ -0,0 +1,48 @@
+package ru.netology.manager;
+
+public class Repository {
+
+    Product[] products = new Product[0];
+
+    public void add(Product product) {
+        Product[] tmp = new Product[products.length + 1];
+        for (int i = 0; i < products.length; i++) {
+            tmp[i] = products[i];
+        }
+        tmp[tmp.length - 1] = product;
+        products = tmp;
+
+    }
+
+    public Product[] findAll() {
+        return products;
+    }
+
+
+    public void remove(int removeId) {
+
+        Product foundProduct = findById(removeId);
+        if (foundProduct == null) {
+            throw new NotFoundException(removeId);
+        }
+
+        Product[] tmp = new Product[products.length - 1];
+        int index = 0;
+        for (Product product : products) {
+            if (product.getId() != removeId) {
+                tmp[index] = product;
+                index++;
+            }
+        }
+        products = tmp;
+    }
+
+    public Product findById(int id) {
+        for (Product product : products)
+            if (product.getId() == id) {
+                return product;
+            }
+        return null;
+    }
+
+}
